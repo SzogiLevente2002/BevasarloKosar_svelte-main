45aaf0c1c309f3eeec040cb08c4de4271ef79379
SzogiLevente2002 <szogileventeattila@gmail.com>
2024-11-13 10:43:19 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  //@ts-nocheck
+    import Kosar from './lib/Kosar.svelte'
+  </script>
+  
+  <main>
+    <div class="card">
+      <Kosar termekek={[
+        {nev:'sajt ömlesztett 100g',ar:2000},
+        {nev:'tejföl 200g tesco',ar:1800},
+      ]}/>
+    </div>
+  </main>
+  
---
+++ src/lib/Kosar.svelte
@@ -0,0 +1,45 @@
+
+<script>
+  export let  termekek
+</script>
+
+<h1>
+  kosár
+</h1>
+<table>
+  <tr>
+    <th>Termék neve</th>
+    <th>Ára</th>
+    <th>Mennyiség</th>
+    </tr>
+    {#each  termekek as item }
+    <tr>
+      <td>{item.nev}</td>
+      <td>{item.ar} Ft</td>
+      <td> <input type="number" min="0">Db</td>
+    </tr>
+    {/each}
+  </table>
+
+  <style>
+th,td {
+  border: 2px solid black;
+}
+
+th {
+  background-color: turquoise;
+
+}
+tr {
+  background-color: greenyellow;
+
+}
+
+h1 {
+  font-size: large;
+  align-items: center;
+}
+
+
+    
+  </style>
